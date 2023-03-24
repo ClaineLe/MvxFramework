472d0b8f13de3ca04d048d1b38e525462182aca4
乐开霖 <7941324@qq.com>
2023-03-24 13:19:56 +0800

    [mvx-source] 修复c#9.0 语法导致引用丢失的编译错误

--- mvvmcross_for_unity3d/Assets/MvxFramework/MvvmCross/MvvmCross@9.0.9/Binding/BindingContext/MvxBindingContext.cs
+++ mvvmcross_for_unity3d/Assets/MvxFramework/MvvmCross/MvvmCross@9.0.9/Binding/BindingContext/MvxBindingContext.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using MvvmCross.Binding.Binders;
 using MvvmCross.Binding.Bindings;
 
