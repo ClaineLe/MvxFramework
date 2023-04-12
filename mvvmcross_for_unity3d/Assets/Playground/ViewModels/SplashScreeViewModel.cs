0353eb770778aa0de4b1b0f65e03f8078375f357
乐开霖 <7941324@qq.com>
2023-04-12 10:48:03 +0800

    [unity.mvx]增加Image.sprite和RawImage.texture的测试用例

--- mvvmcross_for_unity3d/Assets/Playground/ViewModels/SplashScreeViewModel.cs
+++ mvvmcross_for_unity3d/Assets/Playground/ViewModels/SplashScreeViewModel.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Logging;
 using MvvmCross;
 using MvvmCross.Commands;
 using MvvmCross.Localization;
-using MvvmCross.Navigation;
 using MvvmCross.Plugin.JsonLocalization;
 using MvvmCross.ViewModels;
 using Playground.Core;
@@ -10,29 +9,66 @@ using UnityEngine;
 
 namespace Playground.ViewModels
 {
-    public class SplashScreeViewModel : MvxNavigationViewModel
+    public class UnityViewModel : MvxViewModel
+    {
+        public IMvxLanguageBinder TextSource => new MvxLanguageBinder(Constants.GeneralNamespace, GetType().Name);
+    }
+
+    public class SplashScreeViewModel : UnityViewModel
     {
         private IMvxTextProviderBuilder _builder;
         private IMvxTextProviderBuilder builder => _builder ??= Mvx.IoCProvider.Resolve<IMvxTextProviderBuilder>();
 
-        public IMvxLanguageBinder TextSource => new MvxLanguageBinder(Constants.GeneralNamespace, GetType().Name);
-
         public IMvxCommand BtnChineseCommand { get; }
         public IMvxCommand BtnEnglishCommand { get; }
         public IMvxCommand BtnRefreshCommand { get; }
-        
-        public SplashScreeViewModel(ILoggerFactory logFactory, IMvxNavigationService navigationService) : base(
-            logFactory, navigationService)
+        public IMvxCommand BtnSpriteCommand { get; }
+        public IMvxCommand BtnTextureCommand { get; }
+
+        private string _imageAssetKey;
+        public string ImageAssetKey
         {
-            BtnChineseCommand = new MvxCommand(() => PickLanguage(string.Empty));
-            BtnEnglishCommand = new MvxCommand(() => PickLanguage("English"));
-            BtnRefreshCommand = new MvxCommand(() => RaisePropertyChanged(() => TextSource));
+            get => _imageAssetKey;
+            set => SetProperty(ref _imageAssetKey, value);
+        } 
+        
+        private string _rawImageAssetKey;
+        public string RawImageAssetKey
+        {
+            get => _rawImageAssetKey;
+            set => SetProperty(ref _rawImageAssetKey, value);
         }
 
+        public SplashScreeViewModel()
+        {
+            BtnRefreshCommand = new MvxCommand(() => RaisePropertyChanged(() => TextSource));
+            BtnChineseCommand = new MvxCommand(() => PickLanguage(string.Empty));
+            BtnEnglishCommand = new MvxCommand(() => PickLanguage("English"));
+            BtnSpriteCommand = new MvxCommand(NextSprite);
+            BtnTextureCommand = new MvxCommand(NextTexture);
+        }
+ 
         private void PickLanguage(string which)
         {
             Debug.Log($"PickLanguage:{which}");
             builder.LoadResources(which);
+            BtnRefreshCommand.Execute();
+        }
+
+        private int imageIndex = 1;
+        private void NextSprite()
+        {
+            ImageAssetKey = $"d_genericbuhoshechristmas{imageIndex}";
+            if (++imageIndex > 3)
+                imageIndex = 1;
+        }
+        
+        private int rawImageIndex = 1;
+        private void NextTexture()
+        {
+            RawImageAssetKey = $"d_genericbuhoshe{rawImageIndex}";
+            if (++rawImageIndex > 3)
+                rawImageIndex = 1;
         }
     }
 }
--- mvvmcross_for_unity3d/Assets/Playground/Views/SplashScreenWindow.cs
+++ mvvmcross_for_unity3d/Assets/Playground/Views/SplashScreenWindow.cs
@@ -1,3 +1,4 @@
+using MvvmCross.Binding;
 using MvvmCross.Binding.BindingContext;
 using MvxFramework.UnityEngine.Views;
 using Playground.ViewModels;
@@ -10,7 +11,12 @@ namespace Playground.Views
         public Button BtnChinese;
         public Button BtnEnglish;
         public Button BtnRefresh;
+        public Button BtnTexture;
+        public Button BtnSprite;
 
+        public Image ImgBoard;
+        public RawImage RawImgBoard;
+        
         public Text TxtContext;
 
         protected override void OnViewLoaded()
@@ -19,7 +25,13 @@ namespace Playground.Views
             setter.Bind(this.BtnChinese).To(vm => vm.BtnChineseCommand);
             setter.Bind(this.BtnEnglish).To(vm => vm.BtnEnglishCommand);
             setter.Bind(this.BtnRefresh).To(vm => vm.BtnRefreshCommand);
-            this.BindLanguage(TxtContext, "text", "ExampleText");
+            
+            setter.Bind(this.BtnTexture).To(vm => vm.BtnTextureCommand);
+            setter.Bind(this.BtnSprite).To(vm => vm.BtnSpriteCommand);
+            
+            setter.Bind(this.ImgBoard).To(vm => vm.ImageAssetKey);
+            setter.Bind(this.RawImgBoard).To(vm => vm.RawImageAssetKey);
+            this.BindLanguage(TxtContext, "text", "ExampleText", bindingMode: MvxBindingMode.OneWay);
             setter.Apply();
         }
     }
