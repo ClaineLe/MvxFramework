d3be41461b0746c7d2742ad034c040b00d4cd5a8
乐开霖 <7941324@qq.com>
2023-04-21 18:29:07 +0800

    [unity.mvx] 添加语言切换消息定义

---
+++ mvx-framework/Packages/mvx-framework/UnityEngine/Messages/LanguageChangedMessage.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Plugin.Messenger;
+
+
+namespace UnityEngine.Messages
+{
+    public class LanguageChangedMessage : MvxMessage
+    {
+        public LanguageChangedMessage(object sender) : base(sender)
+        {
+        }
+    }
+}
