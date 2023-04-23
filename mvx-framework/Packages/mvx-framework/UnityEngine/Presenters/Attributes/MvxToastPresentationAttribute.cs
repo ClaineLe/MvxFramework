36cc95c8fd3247261a6956b57ac5a930e851c3bf
乐开霖 <7941324@qq.com>
2023-04-23 19:03:25 +0800

    [unity.mvx] 添加ToastPresentation属性

---
+++ mvx-framework/Packages/mvx-framework/UnityEngine/Presenters/Attributes/MvxToastPresentationAttribute.cs
@@ -0,0 +1,9 @@
+namespace MvxFramework.UnityEngine.Presenters.Attributes
+{
+    public class MvxToastPresentationAttribute : MvxContentPresentationAttribute
+    {
+        public MvxToastPresentationAttribute(string assetPath, bool resourceModel = false) : base(assetPath, resourceModel)
+        {
+        }
+    }
+}
