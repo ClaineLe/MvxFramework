681128e2411bfe0058084a77c4abe4123161d86e
乐开霖 <7941324@qq.com>
2023-04-24 14:34:50 +0800

    [unity.mvx] 重新组织UnityViewModel的代码结构

--- mvx-framework/Packages/mvx-framework/UnityEngine/ViewModels/MvxUnityViewModel.cs
+++ mvx-framework/Packages/mvx-framework/UnityEngine/ViewModels/MvxUnityViewModel.cs
@@ -1,25 +1,70 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using MvvmCross;
 using MvvmCross.Localization;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
+using MvvmCross.Plugin.Messenger;
+using MvvmCross.ViewModels;
 using UnityEngine.Services.LocalizeService;
 
 namespace MvxFramework.UnityEngine.ViewModels
 {
-    public abstract class MvxUnityViewModel : MvxMessengerViewModel, IMvxUnityViewModel
+    public abstract class MvxUnityViewModel : MvxViewModel, IMvxUnityViewModel
     {
-        protected ILogger log => MvxLogHost.GetLog(this.GetType().Name);
+        private ILogger? _log;
+
+        protected virtual IMvxNavigationService NavigationService { get; }
+
+        protected virtual ILoggerFactory LoggerFactory { get; }
+
+        protected virtual ILogger Log => _log ??= LoggerFactory.CreateLogger(GetType().Name);
+
 
         private IMvxLocalizeService localizeSvr => Mvx.IoCProvider.Resolve<IMvxLocalizeService>();
 
         public IMvxLanguageBinder TextSource => localizeSvr.GetLanguageBinder();
 
-        public MvxUnityViewModel(ILoggerFactory logFactory, IMvxNavigationService navigationService) : base(logFactory,
-            navigationService)
+        private readonly List<IDisposable> _disposables = new List<IDisposable>();
+        private Lazy<IMvxMessenger> _messenger => new(() => Mvx.Resolve<IMvxMessenger>());
+
+        public MvxUnityViewModel(ILoggerFactory logFactory, IMvxNavigationService navigationService)
         {
+            LoggerFactory = logFactory;
+            NavigationService = navigationService;
             localizeSvr.OnChangedLanguage += OnChangedLanguage;
+        }
+
+        protected virtual void OnChangedLanguage(object sender, EventArgs args)
+            => RaisePropertyChanged(() => TextSource);
+
+        protected void AddDisposable(IDisposable disposable) => _disposables.Add(disposable);
+
+        protected void RemoveDisposable(IDisposable disposable) => _disposables.Remove(disposable);
+
+        public void Publish(MvxMessage message) => _messenger.Value.Publish(message);
+
+        public MvxSubscriptionToken Subscribe<TMessage>(Action<TMessage> action, string tag = null)
+            where TMessage : MvxMessage
+        {
+            var token = _messenger.Value.Subscribe<TMessage>(action, MvxReference.Weak, tag);
+            AddDisposable(token);
+            return token;
+        }
+
+        public MvxSubscriptionToken SubscribeOnMainThread<TMessage>(Action<TMessage> action, string tag = null)
+            where TMessage : MvxMessage
+        {
+            var token = _messenger.Value.SubscribeOnMainThread<TMessage>(action, MvxReference.Weak, tag);
+            AddDisposable(token);
+            return token;
+        }
+
+        public void Unsubscribe(MvxSubscriptionToken token)
+        {
+            RemoveDisposable(token);
+            token.Dispose();
         }
 
         public void CloseSelf()
@@ -27,19 +72,27 @@ namespace MvxFramework.UnityEngine.ViewModels
             this.NavigationService.Close(this);
         }
 
-        protected override void Dispose(bool disposing)
+        ~MvxUnityViewModel()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
                 localizeSvr.OnChangedLanguage -= OnChangedLanguage;
+
+                foreach (var disposable in _disposables)
+                    disposable.Dispose();
+                _disposables.Clear();
             }
-            base.Dispose(disposing);
-        }
-        
-        
-        protected virtual void OnChangedLanguage(object sender, EventArgs args)
-        {
-            RaisePropertyChanged(() => TextSource);
         }
     }
 }
